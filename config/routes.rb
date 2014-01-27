f56d55ebb0c54a258fad30694e0c490bb490d64b
Sergio Márquez <smarquezs@gmail.com>
2014-01-27 16:30:57 -0300

    Add resources sessions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   #Static page
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
